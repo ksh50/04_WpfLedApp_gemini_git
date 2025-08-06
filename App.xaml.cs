67e819c96b145f16458afb3400fb290891fc9958
ksh50 <kishi.katsuhito@gmail.com>
2025-08-06 23:19:39 +0900

    20250806_wpf_defalut

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _04_WpfLedApp_gemini_git;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
